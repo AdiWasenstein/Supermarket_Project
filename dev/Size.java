04288ca2097b9a0ae76a10986a22414097e06d24
Yoav Zelinger <127612922+yoavzelinger@users.noreply.github.com>
2023-04-01 15:11:31 +0300

    Delete Size.java

--- dev/Size.java
+++
@@ -1,9 +0,0 @@
-public class Size {
-    double amount;
-    MeasureUnit type;
-
-    public Size(double amount, MeasureUnit type){
-        this.amount = amount;
-        this.type = type;
-    }
-}
