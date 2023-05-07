47b492ef58da2eec2cd46eaea8cce6ecc24e118b
Yoav Zelinger <yoavzel@post.bgu.ac.il>
2023-05-07 15:23:26 +0300

    Added pair class

---
+++ dev/SuperLi/src/BusinessLogic/Pair.java
@@ -0,0 +1,19 @@
+package SuperLi.src.BusinessLogic;
+
+public class Pair<T, U> {
+    T first;
+    U second;
+    public static <T, U> Pair<T, U> create(T first, U second){
+        return new Pair<>(first, second);
+    }
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+    public T getLeft() {
+        return first;
+    }
+    public U getRight() {
+        return second;
+    }
+}
