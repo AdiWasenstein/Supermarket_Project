eec5786f9b16a937ed0a0a88a55f9cc79e208be7
Yoav Zelinger <yoavzel@post.bgu.ac.il>
2023-05-07 19:28:57 +0300

    Template of database queries

--- dev/SuperLi/src/DataAccess/BranchDataMapper.java
+++ dev/SuperLi/src/DataAccess/BranchDataMapper.java
@@ -6,6 +6,9 @@ import java.util.*;
 public class BranchDataMapper implements IDataMapper<Branch> {
     Map<Integer, Branch> branchIdentityMap;
     static BranchDataMapper branchDataMapper = null;
+    public String insertQuery(Branch b){
+        return String.format("INSERT INTO Branches(Id, Address) VALUES (%d, '%s')", b.getId(), b.getAddress());
+    }
     private BranchDataMapper(){
         branchIdentityMap = new HashMap<>();
     }
@@ -18,7 +21,32 @@ public class BranchDataMapper implements IDataMapper<Branch> {
         return Optional.empty();
     }
     public LinkedList<Branch> findAll(){return new LinkedList<>();}
-    public void insert(Branch object) {}
+    public void insert(Branch object){
+        Connection conn;
+        Statement stmt;
+        try{
+            conn = DriverManager.getConnection("jdbc:sqlite:SuppliersStock.db");
+            stmt = conn.createStatement();
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+            System.out.println("Cannot open the database");
+            return;
+        }
+        try {
+            stmt.executeUpdate(insertQuery(object));
+        }
+        catch (SQLException e){
+            System.out.println("Cannot insert the item");
+            System.out.println(e.getMessage());
+        }
+        try{
+            conn.close();
+        }
+        catch (SQLException e){
+            System.out.println("Failed to close the connection");
+        }
+    }
     public void update(Branch object) {}
     public void delete(Branch object) {}
 }
