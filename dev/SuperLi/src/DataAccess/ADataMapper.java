c808c04f34522d94afdae3e3c64cd2a64f6d1aa8
Itzak Elbazis <127624982+elbazis@users.noreply.github.com>
2023-05-08 15:53:20 +0300

    Update all void querys

--- dev/SuperLi/src/DataAccess/ADataMapper.java
+++ dev/SuperLi/src/DataAccess/ADataMapper.java
@@ -10,14 +10,8 @@ public abstract class ADataMapper<T> {
     public abstract String deleteQuery(T object);
     public abstract String updateQuery(T object);
 
-    public void executeQuery(Function<T, String> func, T object){
-
-    }
-    public void insert(T object){
-        executeQuery(insertQuery, object);
-    }
-    public void insert(T object){
-        Connection conn;
+    public void executeVoidQuery(Function<T, String> queryFunc, T object){
+		Connection conn;
         Statement stmt;
         try{
             conn = DriverManager.getConnection("jdbc:sqlite:SuppliersStock.db");
@@ -28,7 +22,7 @@ public abstract class ADataMapper<T> {
         }
         try{
             stmt = conn.createStatement();
-            stmt.executeUpdate(insertQuery(object));
+            stmt.executeUpdate(queryFunc(object));
         }
         catch (SQLException e){
             System.out.println(e.getMessage());
@@ -40,11 +34,14 @@ public abstract class ADataMapper<T> {
             System.out.println(e.getMessage());
         }
     }
+    public void insert(T object){
+        executeVoidQuery(insertQuery, object);
+    }
     void update(T object){
-
+		executeVoidQuery(updateQuery, object);
     }
     void delete(T object){
-
+		executeVoidQuery(deleteQuery, object);
     }
     Optional<T> find(String param){return null;}
 }
