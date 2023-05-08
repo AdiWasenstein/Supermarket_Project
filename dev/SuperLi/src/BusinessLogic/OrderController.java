b95802d1d56d9617c13053e81a0a122277bf1fdc
tiltang <127848200+tiltang@users.noreply.github.com>
2023-05-08 15:30:08 +0300

    fastest help functions

--- dev/SuperLi/src/BusinessLogic/OrderController.java
+++ dev/SuperLi/src/BusinessLogic/OrderController.java
@@ -1,17 +1,28 @@
 package SuperLi.src.BusinessLogic;
 
 import SuperLi.src.DataAccess.OrderDataMapper;
+import SuperLi.src.DataAccess.PeriodicReportDataMapper;
+import SuperLi.src.DataAccess.BranchDataMapper;
 //import org.graalvm.collections.Pair;
 import SuperLi.src.BusinessLogic.Pair;
 
 
 import java.util.LinkedList;
+import java.util.Optional;
 
 public class OrderController {
     private static OrderController instance = new OrderController();
     private OrderDataMapper orderDataMapper;
+//    private BranchDataMapper branchDataMapper;
+    private PeriodicReportDataMapper periodicReportDataMapper;
 
-    private OrderController() { this.orderDataMapper = OrderDataMapper.getInstance();
+
+
+    private OrderController()
+    {
+        this.orderDataMapper = OrderDataMapper.getInstance();
+//        this.branchDataMapper = BranchDataMapper.getInstance();
+        this.periodicReportDataMapper = PeriodicReportDataMapper.getInstance();
     }
 
     public static OrderController getInstance(){return instance;}
@@ -48,6 +59,7 @@ public class OrderController {
         return null;
     }
 
+    // maybe not necessary
     public PeriodicReport getReportById (int reportNumber)
     {
         // need to check that exist
@@ -62,4 +74,21 @@ public class OrderController {
         // check if amount ok
         return true;
     }
+
+    // TODO maybe void- depends if we just print it or returns it
+    // TODO maybe parameter is string- depends on how we want to receive it from user
+    // given branch number, returnsqprint all the reports of that branch
+    public LinkedList<PeriodicReport> findReportsOfBranch(Integer branchID)
+    {
+        if (branchID <= 0)
+            return null;
+//       Optional<Branch> branchFound = this.branchDataMapper.find(branchID.toString());
+//       // check if branch with the given id is found. If not returns null, else get it.
+//       if (branchFound.isEmpty())
+//           return null;
+        LinkedList<PeriodicReport> reportsFound = this.periodicReportDataMapper.findAll(branchID.toString());
+        return reportsFound;
+
+    }
+
 }
